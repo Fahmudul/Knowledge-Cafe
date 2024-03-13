d6b6d92fcad39e16c5bb8fa65133d06db977a8f7
Fahmudul Hassan <fahmudul7890@email.com>
2024-03-13 10:44:01 +0600

    created header and blog with data

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Blogs from "./components/Blogs/Blogs";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  );
+}
+
+export default App;
